36dc6c712a376d2d30fa01424f310082c2d241f0
Egor <EgorrKosolapoff@ya.ru>
2023-07-21 20:14:41 +0700

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+
+/* void f(double a)
+{
+Console.Write("введите степень числа: ");
+int b = Convert.ToInt32(Console.ReadLine());
+a = Math.Pow(a, b);
+Console.WriteLine(a);
+}
+Console.Clear();
+Console.Write("введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+f(a);
+*/
